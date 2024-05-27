d4c92cee3e46139442e47554e3352afa708f0ed9
uo287585 <uo287585@uniovi.es>
2024-05-27 19:36:47 +0200

    audio noise unlearning

---
+++ AudioNoiseModel.py
@@ -0,0 +1,36 @@
+from audioUnlearning.data import DatasetCreation, MyModel
+import tensorflow as tf
+
+from audioUnlearning.model import EvaluationMethods, SaveMethods, TrainMethods
+from unlearning.evaluation import ConfusionMatrix
+
+#Training the model
+X,X_train,X_test,y_train,y_test = DatasetCreation.get_dataset()
+model = MyModel.get_model(X)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.000146)
+model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+epochs=600
+model_history = TrainMethods.train_model_noise(model, epochs, optimizer, X_train,y_train,X_test,y_test)
+
+#Evaluating the model
+test_loss,test_acc=model.evaluate(X_test,y_test,batch_size=256)
+print("The test loss is ",test_loss)
+print("The best accuracy is: ",test_acc*100)
+
+predicted_index = EvaluationMethods.testing(X_test,model,y_test)
+ConfusionMatrix.plot_confusion_matrix(y_test,predicted_index)
+
+#Saving the model
+SaveMethods.save_model(model,'audioNoise')
+
+#Altered dataset
+X2,X_train2,X_test2,y_train2,y_test2=DatasetCreation.altered_dataset()
+
+#Testing with altered dataset
+test_loss,test_acc=model.evaluate(X_test2,y_test2,batch_size=256)
+print("The test loss is ",test_loss)
+print("The best accuracy is: ",test_acc*100)
+
+predicted_index = EvaluationMethods.testing(X_test2,model,y_test2)
+ConfusionMatrix.plot_confusion_matrix(y_test2,predicted_index)
--- audioUnlearning/model.py
+++ audioUnlearning/model.py
@@ -9,6 +9,21 @@ class TrainMethods():
         model.compile(optimizer=optimizer,loss='sparse_categorical_crossentropy',metrics='accuracy')
         return model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=epochs,batch_size=batch_size)
     
+    def add_noise(x, noise_factor=0.05):
+        noise = np.random.normal(loc=0.0, scale=1.0, size=x.shape)
+        noisy_x = x + noise_factor * noise
+        return np.clip(noisy_x, -1.0, 1.0) 
+    
+    def train_model_noise(model, epochs, optimizer, X_train,y_train,X_test,y_test,noise_factor=3.0):
+        batch_size = 256
+        model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        
+        # Data augmentation: Adding noise to input samples during training
+        noisy_X_train = TrainMethods.add_noise(X_train, noise_factor=noise_factor)
+        
+        return model.fit(noisy_X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size)
+
+    
 class EvaluationMethods():
     def testing(X_test,model,y_test):
         sample = X_test
