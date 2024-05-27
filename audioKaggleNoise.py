d8a174ccc750d828490a9412aa85106bbfd19e2e
uo287585 <uo287585@uniovi.es>
2024-05-27 17:15:42 +0200

    audio model is trained with noise

---
+++ audioKaggleNoise.py
@@ -0,0 +1,137 @@
+import keras
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import scipy
+import os
+import pickle
+import librosa
+import librosa.display
+import IPython.display as ipd
+from IPython.display import Audio
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+
+# Reading the csv file
+df = pd.read_csv("./GTZAN/features_3_sec.csv")
+df.head()
+
+
+#Data PreProcessing
+
+# Label Encoding - encod the categorical classes with numerical integer values for training
+
+# Blues - 0
+# Classical - 1
+# Country - 2
+# Disco - 3
+# Hip-hop - 4 
+# Jazz - 5  
+# Metal - 6 
+# Pop - 7
+# Reggae - 8
+# Rock - 9
+
+class_encod=df.iloc[:,-1]
+converter=LabelEncoder()
+y=converter.fit_transform(class_encod)
+
+
+
+# Drop the column filename as it is no longer required for training
+df=df.drop(labels="filename",axis=1)
+
+#scaling
+from sklearn.preprocessing import StandardScaler
+fit=StandardScaler()
+X=fit.fit_transform(np.array(df.iloc[:,:-1],dtype=float))
+
+# splitting 70% data into training set and the remaining 30% to test set
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3)
+
+# Training the model using the following parameters
+# metrics = accuracy
+# epochs = 600
+# loss = sparse_categorical_crossentropy
+# batch_size = 256
+# optimizer = adam
+
+def add_noise(x, noise_factor=0.05):
+    noise = np.random.normal(loc=0.0, scale=1.0, size=x.shape)
+    noisy_x = x + noise_factor * noise
+    return np.clip(noisy_x, -1.0, 1.0)  # Clip values to stay within the valid range
+
+# Training the model with data augmentation (adding noise)
+def train_model(model, epochs, optimizer, noise_factor=3.0):
+    batch_size = 256
+    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    # Data augmentation: Adding noise to input samples during training
+    noisy_X_train = add_noise(X_train, noise_factor=noise_factor)
+    
+    return model.fit(noisy_X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=batch_size)
+
+# We used different layers to train the neural network by importing keras library from tensorflow framework 
+# for input and hidden neurons we use the most widly used activation function which is relu where as for output neurons we uses softmax activation function
+model=tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(X.shape[1],)),
+    tf.keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(512,activation='relu'),
+    keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(256,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(64,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(32,activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    
+    tf.keras.layers.Dense(10,activation='softmax'),
+])
+
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.000146)
+model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+model_history = train_model(model=model, epochs=600, optimizer=optimizer)
+
+
+
+test_loss,test_acc=model.evaluate(X_test,y_test,batch_size=256)
+print("The test loss is ",test_loss)
+print("The best accuracy is: ",test_acc*100)
+
+
+
+# Sample testing
+sample = X_test
+sample = sample[np.newaxis, ...]
+prediction = model.predict(X_test)
+predicted_index = np.argmax(prediction, axis = 1)
+print("Expected Index: {}, Predicted Index: {}".format(y_test, predicted_index))
+
+# Plotting the confusion matrix for analizing the true positives and negatives
+import seaborn as sns
+import matplotlib.pyplot as plt
+pred_x = model.predict(X_test)
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_test,predicted_index )
+
+# Plot confusion matrix as heatmap
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=np.arange(10), yticklabels=np.arange(10))
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.show()
+
+model.save('./model/audioNoise.keras')
+model.save('./model/audioNoise.h5')
+print('model saved as audio')
